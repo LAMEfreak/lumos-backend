3003829ab67d2708ff653e9158240f60c87afb54
LAMEfreak <leeweirong@outlook.com>
2024-06-01 09:00:29 +0800

    Fix seed data

--- db/seeders/20240601001528-seed-rounds.js
+++ db/seeders/20240601001528-seed-rounds.js
@@ -3,31 +3,29 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    "rounds",
-      [
-        {
-          stage: "Seed",
-          target: 500000,
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          stage: "Pre-Seed",
-          target: 100000,
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          stage: "Series A",
-          target: 2000000,
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ];
+    await queryInterface.bulkInsert("rounds", [
+      {
+        stage: "Seed",
+        target: 500000,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        stage: "Pre-Seed",
+        target: 100000,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        stage: "Series A",
+        target: 2000000,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]);
   },
 
   async down(queryInterface, Sequelize) {
-        await queryInterface.bulkDelete("rounds", null, {});
-
+    await queryInterface.bulkDelete("rounds", null, {});
   },
 };
