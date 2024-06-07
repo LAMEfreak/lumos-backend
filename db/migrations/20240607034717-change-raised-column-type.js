68e6964fd4032f4626b18b59ccbeb75b50efa9b5
LAMEfreak <leeweirong@outlook.com>
2024-06-07 12:56:37 +0800

    Before updating migration

--- db/migrations/20240607034717-change-raised-column-type.js
+++ db/migrations/20240607034717-change-raised-column-type.js
@@ -3,18 +3,12 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("round_investors", "raised");
     await queryInterface.addColumn("round_investors", "raised", {
       type: Sequelize.INTEGER,
-      allowNull: true,
     });
   },
 
   async down(queryInterface, Sequelize) {
     await queryInterface.removeColumn("round_investors", "raised");
-    await queryInterface.addColumn("round_investors", "raised", {
-      type: Sequelize.STRING,
-      allowNull: true,
-    });
   },
 };
