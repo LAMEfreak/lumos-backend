de052b5d84ff9e89a433f2adeaf0c6804b6599c8
LAMEfreak <leeweirong@outlook.com>
2024-06-12 02:04:54 +0800

    Set up Stream API, retrieve token success

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+// =============== STREAM ================= //
+
+const { StreamClient } = require("@stream-io/node-sdk");
+const checkJwt = require("./utilities");
+
+// =============== STREAM ================= //
+
 // importing Routers that define the url endpoints for the frontend to consume
 const InvestorsRouter = require("./routers/investorsRouter");
 const StartupsRouter = require("./routers/startupsRouter");
@@ -45,6 +52,44 @@ app.use(express.urlencoded({ extended: false }));
 // Middleware passes request to the appropriate router
 app.use("/startup", startupsRouter);
 app.use("/investors", investorsRouter);
+
+// =============== STREAM ================= //
+
+const apiKey = process.env.STREAM_API_KEY;
+const secret = process.env.STREAM_SECRET_KEY;
+// optionally add timeout to API requests
+// the default timeout is 3000ms
+client = new StreamClient(apiKey, secret, { timeout: 3000 });
+
+app.post("/stream/:userId", checkJwt, async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const { name, image } = req.body;
+    const newUser = {
+      id: userId,
+      role: "user",
+      name,
+      image,
+    };
+    await client.upsertUsers({
+      users: {
+        [newUser.id]: newUser,
+      },
+    });
+    // Set expiration time to 1 hour
+    const expirationTime = Math.floor(Date.now() / 1000) + 60 * 60;
+    // account for slight discrepancies in system clocks between different servers or systems
+    // providing a buffer to ensure that the "issued at" time is not in the future from the perspective of any system verifying the token.
+    const issuedAt = Math.floor(Date.now() / 1000) - 60;
+    const token = client.createToken(userId, expirationTime, issuedAt);
+    console.log("STREAM TOKEN", token);
+    res.json(token);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// =============== STREAM ================= //
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
